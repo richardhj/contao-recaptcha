ed69851b8a53c3ef91a271893ec05455a13f2b90
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2018-05-16 17:42:37 +0200

    Add lang

---
+++ src/Resources/contao/languages/en/tl_form_field.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * This file is part of richardhj/contao-recaptcha.
+ *
+ * Copyright (c) 2018-2018 Richard Henkenjohann
+ *
+ * @package   richardhj/contao-recaptcha
+ * @author    Richard Henkenjohann <richardhenkenjohann@googlemail.com>
+ * @copyright 2018-2018 Richard Henkenjohann
+ * @license   https://github.com/richardhj/contao-recaptcha/blob/master/LICENSE LGPL-3.0
+ */
+
+$GLOBALS['TL_LANG']['FFL']['recaptcha'][0] = 'reCAPTCHA';
+$GLOBALS['TL_LANG']['FFL']['recaptcha'][1] = 'Binden Sie das Google reCAPTCHA ein.';
